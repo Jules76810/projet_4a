4c13a6e428f96818f3f5c8b02c213433d8d8d3f9
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-02 22:16:58 +0200

    Update 3_📈_Score_unique_EF.py

--- pages/3_📈_Score_unique_EF.py
+++ pages/3_📈_Score_unique_EF.py
@@ -16,6 +16,15 @@ st.set_page_config(page_title="Score unique EF", initial_sidebar_state='auto', l
 data = pd.read_csv('Agribalyse_Synthese (2).csv')
 cols = list(data.columns)
 data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "effets_toxico_cancer"})
+
+st.title('Analyse des Données avec Streamlit')
+st.subheader('Exploration des données avec un Histogramme')
+st.write("Voici un aperçu des données utilisées pour créer l'histogramme :")
+st.dataframe(data)
+fig = px.histogram(data, x="Score Unique EF", title="Histogramme de Score Unique EF")
+st.plotly_chart(fig)
+st.write("Cet histogramme montre la distribution des valeurs de la colonne "Score Unique EF".Les barres représentent la fréquence des différentes valeurs dans l'ensemble des données.")
+
 st.header('Qualité de la donnée:')
 dqr_value = st.select_slider('Qualité de la donné',
     options=[1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])

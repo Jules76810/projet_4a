7b2be46c38ef7aa52d74de83117eb54131a81f9d
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-02 20:28:40 +0200

    Update 2_📊_Vision_génerale.py

--- pages/2_📊_Vision_génerale.py
+++ pages/2_📊_Vision_génerale.py
@@ -36,6 +36,10 @@ st.image("https://autrecuisine.fr/warehouse/cache/large/poster_5f733477321f3.jpg
 st.header('Qualité de la donnée:')
 st.write("Étude du paramètre de la variable DQR - Data Quality Ratio, elle évalue la fiabilité des données et préconise une utilisation des données les plus fiables.")
 st.dataframe(data.describe()['DQR'])
+st.titres = ['mean', 'std', 'min', '25%', '50%', '75%', 'max']
+st.plt.figure(figsize=(12,6))
+st.plt.bar(titres, data.describe()['DQR'][1:].values)
+st.plt.show()
 st.write("Il est conseillé de prendre les valeurs avec un DQR inférieur à 3 afin d'utiliser les valeurs les plus fiables, selon la Commission Européenne.")
 st.divider()
 

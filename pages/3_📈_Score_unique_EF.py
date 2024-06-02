c14a1d2934ceeb81177b1ba652ce983ac7dc6eed
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-02 23:06:47 +0200

    Update 3_📈_Score_unique_EF.py

--- pages/3_📈_Score_unique_EF.py
+++ pages/3_📈_Score_unique_EF.py
@@ -57,6 +57,7 @@ top_variables = pd.DataFrame(df.corr()['Score unique EF'].sort_values(ascending=
 top_variables = top_variables.reset_index()
 top_variables = top_variables.iloc[1:, :]
 st.dataframe(top_variables)
+st.write("Choix de travailler avec les 5 variables les plus corrélées avec le score unique EF malgré le fait que ce ne soit pas les variables qui possèdent le plus de pondération dans le calcul de ce dernier.")
 
 st.sidebar.title('À propos')
 st.sidebar.info('Cette application a été développée par Margaux BOYER, Marion DE CACQUERAY, Jules LEFORT et Laure WATERHOUSE.')

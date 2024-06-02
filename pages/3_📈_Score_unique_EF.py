f6ba9e35b69c1503e271053f616b18e73447cd4f
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-02 22:23:47 +0200

    Update 3_📈_Score_unique_EF.py

--- pages/3_📈_Score_unique_EF.py
+++ pages/3_📈_Score_unique_EF.py
@@ -22,7 +22,8 @@ data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "eff
 st.title('Analyse des Données avec Streamlit')
 st.subheader('Exploration des données avec un Histogramme')
 st.write("Voici un aperçu des données utilisées pour créer l'histogramme :")
-fig = px.histogram(data, x="Score Unique EF", title="Histogramme de Score Unique EF")
+st.dataframe(data)
+fig = px.histogram(data, x="Score unique EF", title="Histogramme de Score Unique EF")
 st.plotly_chart(fig)
 st.write("Cet histogramme montre la distribution des valeurs de la colonne Score Unique EF. Les barres représentent la fréquence des différentes valeurs dans l'ensemble des données.")
 

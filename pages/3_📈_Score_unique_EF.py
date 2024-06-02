7dd8dd68222f204cee56053e828215dfcec44815
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-02 22:43:38 +0200

    Update 3_📈_Score_unique_EF.py

--- pages/3_📈_Score_unique_EF.py
+++ pages/3_📈_Score_unique_EF.py
@@ -17,7 +17,8 @@ data = pd.read_csv('Agribalyse_Synthese (2).csv')
 cols = list(data.columns)
 data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "effets_toxico_cancer"})
 
-
+st.write("Le score unique EF représente le score préconisé par la Commission Européenne qui pondère les différents indicateurs ayant une influence environnementale. On peut notamment voir la pondération des différents facteurs sur le tableau ci-dessous.")
+st.image("https://doc.agribalyse.fr/~gitbook/image?url=https%3A%2F%2F2407839794-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-legacy-files%2Fo%2Fassets%252F-LpO7Agg1DbhEBNAvmHP%252F-MLmGl1xWNUdFGoOBo9f%252F-MLmGoORnso9M46c0PtM%252Fjrc.png%3Falt%3Dmedia%26token%3D9da03594-5db3-433b-9461-c24e23ee5e1c&width=768&dpr=2&quality=100&sign=4fe1c17ceedc8702a665f9d5c511f618313f5439678e76fc2ef954da1cd9c621")
 
 st.title('Analyse des Données avec Streamlit')
 st.subheader('Exploration des données avec un Histogramme')
@@ -27,19 +28,7 @@ fig = px.histogram(data, x="Score unique EF", title="Histogramme de Score Unique
 st.plotly_chart(fig)
 st.write("Cet histogramme montre la distribution des valeurs de la colonne Score Unique EF. Les barres représentent la fréquence des différentes valeurs dans l'ensemble des données.")
 
-st.header('Qualité de la donnée:')
-dqr_value = st.select_slider('Qualité de la donné',
-    options=[1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
-st.write('Donnés avec DQR plus petit que:', dqr_value)
-df = data[data["DQR"]<dqr_value]
 
-st.header('Le score unique EF')
-
-st.line_chart(df['Score unique EF'])
-min_EF, max_EF, mean_EF = st.columns(3)
-min_EF.metric("Min", np.round(df['Score unique EF'].min(), 3))
-max_EF.metric("Max", np.round(df['Score unique EF'].max(), 3))
-mean_EF.metric("Moyenne", np.round(df['Score unique EF'].mean(), 3))
 
 st.sidebar.title('À propos')
 st.sidebar.info('Cette application a été développée par Margaux BOYER, Marion DE CACQUERAY, Jules LEFORT et Laure WATERHOUSE.')

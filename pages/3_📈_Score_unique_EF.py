c06a0aae5bca13c12331c24c94c8f0712d475469
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-02 22:45:47 +0200

    Update 3_📈_Score_unique_EF.py

--- pages/3_📈_Score_unique_EF.py
+++ pages/3_📈_Score_unique_EF.py
@@ -17,7 +17,7 @@ data = pd.read_csv('Agribalyse_Synthese (2).csv')
 cols = list(data.columns)
 data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "effets_toxico_cancer"})
 
-st.header("Le score unique EF.")
+st.header("Le score unique EF")
 st.write("Le score unique EF représente le score préconisé par la Commission Européenne qui pondère les différents indicateurs ayant une influence environnementale. On peut notamment voir la pondération des différents facteurs sur le tableau ci-dessous.")
 st.image("https://doc.agribalyse.fr/~gitbook/image?url=https%3A%2F%2F2407839794-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-legacy-files%2Fo%2Fassets%252F-LpO7Agg1DbhEBNAvmHP%252F-MLmGl1xWNUdFGoOBo9f%252F-MLmGoORnso9M46c0PtM%252Fjrc.png%3Falt%3Dmedia%26token%3D9da03594-5db3-433b-9461-c24e23ee5e1c&width=768&dpr=2&quality=100&sign=4fe1c17ceedc8702a665f9d5c511f618313f5439678e76fc2ef954da1cd9c621")
 

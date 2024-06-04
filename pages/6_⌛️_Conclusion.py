3ef839ec8c0f84be5d246ed7e6bfb5fb598bbe79
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-04 09:47:10 +0200

    Create 6_⌛️_Conclusion.py

---
+++ pages/6_⌛️_Conclusion.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+from sklearn import preprocessing
+import plotly.graph_objects as go
+from sklearn.cluster import KMeans
+
+st.set_page_config(page_title="Conclusion", initial_sidebar_state='auto', layout="wide")
+st.set_page_config(page_title="Score unique EF", initial_sidebar_state='auto', layout="wide")
+data = pd.read_csv('Agribalyse_Synthese (2).csv')
+cols = list(data.columns)
+data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "effets_toxico_cancer"})
+
+st.header("Conclusion")
+
+
+
+
+
+st.sidebar.title('À propos')
+st.sidebar.info('Cette application a été développée par Margaux BOYER, Marion DE CACQUERAY, Jules LEFORT et Laure WATERHOUSE.')

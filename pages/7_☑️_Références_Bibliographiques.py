bd7ccd8282204e0190deb25b31cef4ce7c0995c2
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-04 23:11:25 +0200

    Update 7_☑️_Références_Bibliographiques.py

--- pages/7_☑️_Références_Bibliographiques.py
+++ pages/7_☑️_Références_Bibliographiques.py
@@ -16,6 +16,16 @@ st.set_page_config(page_title="Références Bibligraphiques", initial_sidebar_st
 st.title('Projet 4A - Traitement de données alimentaires')
 st.title('Références Bibliographiques')
 
+st.write("""
+Accès aux données | Documentation AGRIBALYSE®, 2024. [en ligne]. Date de consultation : 04/06/2024. Disponible sur : <https://doc.agribalyse.fr/documentation/utiliser-agribalyse/acces-donnees>
+Analyse du Cycle de Vie ACV, In eco-conception.fr [en ligne]. Date de consultation : 04/06/2024. Disponible sur : <https://www.eco-conception.fr/static/analyse-du-cycle-de-vie-acv.html>
+Google Colab, [en ligne]. Date de consultation : 04/06/2024. Disponible sur : <https://colab.research.google.com/drive/1r5EBrZhbMDmGd651fpRODIATcYk4KucG?usp=sharing#scrollTo=I5Uvn0ubelFi>
+Graphique: Marché de l’alimentation : quels produits génèrent le plus de revenus ? | Statista, [en ligne]. Date de consultation : 04/06/2024. Disponible sur : <https://fr.statista.com/infographie/24237/segments-alimentaires-qui-generent-le-plus-de-revenus-part-du-chiffre-affaires-mondial/>
+Infographie: Marché de l’alimentation : quels produits génèrent le plus de revenus ?, 2024. In Statista Daily Data [en ligne]. Date de consultation : 04/06/2024. Disponible sur : <https://fr.statista.com/infographie/24237/segments-alimentaires-qui-generent-le-plus-de-revenus-part-du-chiffre-affaires-mondial>
+Jules76810, 2024. Jules76810/projet_4a. Date de consultation : 04/06/2024. Disponible sur : <https://github.com/Jules76810/projet_4a>.
+Schwab, P.-N., 2017. Étude de marché : les évolutions du marché de l’alimentation. Conseils en marketingDate de consultation : 04/06/2024. Disponible sur : <https://www.intotheminds.com/blog/etude-de-marche-france-evolutions-marche-de-alimentation/>.
+Streamlit, [en ligne]. Date de consultation : 04/06/2024. Disponible sur : <https://projetagribalyse4a.streamlit.app/R%C3%A9f%C3%A9rences_Bibliographiques>
+""")
 
 
 

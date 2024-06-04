209664f00a6ddcbb73b262e6bdee59f3439d6034
Jules76810 <jules.lefort@etu.unilasalle.fr>
2024-06-04 20:25:31 +0200

    Update 6_⌛️_Conclusion.py

--- pages/6_⌛️_Conclusion.py
+++ pages/6_⌛️_Conclusion.py
@@ -19,6 +19,11 @@ data = data.rename(columns={cols[18]: "effets_toxico_non_cancer", cols[19]: "eff
 st.title('Projet 4A - Traitement de données alimentaires')
 st.header("Conclusion")
 
+st.write("En conclusion, la base données agribalyse est une base de données complète qui peut servir d’outils dans différents secteurs comme : l’agriculture, les consommateurs, le secteur de la restauration ou encore pour la recherche et l’enseignement par exemple. Durant ce projet, l’impact environnementale de certains produits a été mis en lumière. Dans la plupart des cas, des différences majeures ont pu être observées entre les produits transformés et ceux non transformés.  
+
+Le fait de se baser sur l’analyse du cycle de vie des produits, permet d’identifier les étapes plus gourmandes, en eau ou encore en énergie. L’identification de ces étapes est primordiale pour pouvoir ensuite diminuer ces utilisations de ressources et ainsi essayer de réduire les impacts environnementaux.  
+
+Il a été constaté au début de notre travail que les 5 indicateurs ayant les plus grands scores EF n’était pas forcément ceux qui étaient les plus corrélés. ")
 
 st.divider()
 st.write("**Merci d'avoir suivi notre projet, nous vous invitons désormais à revenir à la page d'accueil avec le lien ci-dessous.**")
